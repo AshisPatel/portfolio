b1966aed6ff7e3ffd4046a2d64741937a07be760
AP <ashis.n.patel@gmail.com>
2021-10-24 17:48:01 -0500

    Add filler projectCard

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -1,9 +1,16 @@
 import React from "react";
 
-function ProjectCard() {
+function ProjectCard({ project, index }) {
+    const { name, description, image } = project; 
+
+    const position = index % 2 === 0 ? "row justify-content-start" : "row justify-content-end"
+
     return (
-        <article>
-            This is a project.
+        <article className={`${position} projectCard`}>
+            <div className="col-6">
+                <h3>Temp Project Title</h3>
+                <p>Temporary Project Description</p>
+            </div>
         </article>
     );
 }
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -1,11 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ProjectCard from "./ProjectCard";
 
 function Projects(props) {
+
+    const completed = [
+        {
+            name: "Gripe Support",
+            description: "SUPPORT BY GRIPING, GRIPE BY SUPPORTING - THAT'S NOT RIGHT Dx",
+            image: ""
+        },
+        {
+            name: "Destressing Homepage",
+            description: "Ayyo homie, just chill :3",
+            image: ""
+        },
+        {
+            name: "Calorie",
+            description: "LET'S GET THIS BREAD BABY, TRACK THOSE CALORIES, TRACK THOSE HANDSSSSSS",
+            image: ""
+        },
+    ];
+
+    const developing = [
+        {
+            name: "Cozy",
+            description: "IT'S LIKE THE DESTRESSING HOME PAGE BUT MORE BALLER? MERN STACKED",
+            image: ""
+        },
+        {
+            name: "Some type of way",
+            description: "CAN YOU FEEL IT BB?",
+            image: ""
+        },
+        {
+            name: "incremental game",
+            description: "I am my own problems",
+            image: ""
+        },
+        {
+            name: "MONEY MOVES",
+            description: "We can make that cash money, lets get this sheet",
+            image: ""
+        }
+    ];
+
+    const [viewCompleted, setViewCompleted] = useState(true); 
+
+
     return (
-        <section>
-            Projects Section
+        <section className="container">
+            <ProjectCard project={{name: "name", description: "description", image: ""}} index={1}/>
         </section>
     );
 }
 
-export default Projects; 
+export default Projects;
