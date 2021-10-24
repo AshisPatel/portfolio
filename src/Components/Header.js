5276147138425bb20bdfc8599555fba1ab659959
AP <ashis.n.patel@gmail.com>
2021-10-24 10:49:33 -0500

    Add navbar and basic outline header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,24 @@ import React from "react";
 
 function Header(props) {
     return (
-        <section>
-            Header Section
+        <section className="d-flex justify-content-between pt-2">
+            
+            <h2 id="home" className="ps-3">
+                Ashis Patel 
+            </h2>
+
+            <nav>
+                <ul className="list-group list-group-horizontal me-3">
+                    <li className="dropdown px-4 py-2 me-1">Projects
+                        <div className="dropdown-content mt-2 p-2">
+                            <p>Completed</p>
+                            <p>Developing</p>
+                        </div>
+                    </li>
+                    <li className="px-4 py-2 me-1">About Me</li>
+                    <li className="px-4 py-2">Contact</li>
+                </ul>
+            </nav>
         </section>
     );
 }
