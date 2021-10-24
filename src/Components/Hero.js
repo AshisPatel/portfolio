614aed460416b8f82d415892a8b5eddb7a33fc31
AP <ashis.n.patel@gmail.com>
2021-10-24 16:02:27 -0500

    Add generic intro snippet to hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -4,11 +4,11 @@ function Hero(props) {
     return (
         <section id="hero-wrapper" className="my-3">
             <div class="d-flex flex-column w-75 mx-auto py-5 text-center">
-                <h1>Hello, Welcome to My Portfolio!</h1>
-                <h4> My name is Ashis, grab yourself something nice to drink and enjoy your visit! (^^)b</h4>
-                <div >
-                
-                </div>
+                <h1>Hello friend! My name is Ashis Patel.</h1>
+                <p>
+                    I am a Full Stack Developer that is passionate about creating web applications that improve the lives of users! I have a background in consulting and teaching, where I have solved hundreds of engineering problems, taught dozens of classes and even developed my own training course. I've created many happy clients in my career and I look forward to continue doing so in the field of web development! 
+
+                </p>
                 <div>
                     <button>About Me</button>
                     <button>Projects</button>
