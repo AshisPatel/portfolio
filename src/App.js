99f40c3ffee4237b872e58a9be29811beaaddf39
AP <ashis.n.patel@gmail.com>
2021-10-24 12:04:31 -0500

    Add filler content to hero with coffee-cup animation

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Header from "./Components/Header";
+import Hero from "./Components/Hero";
 import About from "./Components/About";
 import Projects from "./Components/Projects";
 import Footer from "./Components/Footer";
@@ -8,6 +9,7 @@ function App() {
   return (
   <main>
     <Header />
+    <Hero />
     <Projects />
     <About />
     <Footer />
