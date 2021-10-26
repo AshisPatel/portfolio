402924d32a78097594cb0d7131dd25c4d38d6811
AshisPatel <ashis.n.patel@gmail.com>
2021-10-26 11:48:35 -0500

    Format project card text info

--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -6,34 +6,52 @@ function Projects(props) {
     const completed = [
         {
             name: "Gripe Support",
-            description: "SUPPORT BY GRIPING, GRIPE BY SUPPORTING - THAT'S NOT RIGHT Dx",
-            image: ""
-        },
-        {
-            name: "Destressing Homepage",
-            description: "Ayyo homie, just chill :3",
-            image: ""
-        },
-        {
-            name: "Calorie",
-            description: "LET'S GET THIS BREAD BABY, TRACK THOSE CALORIES, TRACK THOSE HANDSSSSSS",
-            image: ""
+            technology: "Sequelize, Handlebars, Express, Node",
+            description: "Gripe support was designed as a space where one could gripe or support other user's anonymously. Visitors can create an account that will allow them to make posts (categorized as a gripe or an encouragement). Additionally, user's can 'hug' and comment on posts.",
+            image: "gs",
+            website: "https://infinite-bayou-42393.herokuapp.com/",
+            github: "https://github.com/wsbousman/Gripe-Support"
         },
         {
             name: "Gripe Support",
-            description: "SUPPORT BY GRIPING, GRIPE BY SUPPORTING - THAT'S NOT RIGHT Dx",
-            image: ""
+            technology: "Sequelize, Handlebars, Express, Node",
+            description: "Gripe support was designed as a space where one could gripe or support other user's anonymously. Visitors can create an account that will allow them to make posts (categorized as a gripe or an encouragement). Additionally, user's can 'hug' and comment on posts.",
+            image: "gs",
+            website: "https://infinite-bayou-42393.herokuapp.com/",
+            github: "https://github.com/wsbousman/Gripe-Support"
         },
         {
-            name: "Destressing Homepage",
-            description: "Ayyo homie, just chill :3",
-            image: ""
+            name: "Gripe Support",
+            technology: "Sequelize, Handlebars, Express, Node",
+            description: "Gripe support was designed as a space where one could gripe or support other user's anonymously. Visitors can create an account that will allow them to make posts (categorized as a gripe or an encouragement). Additionally, user's can 'hug' and comment on posts.",
+            image: "gs",
+            website: "https://infinite-bayou-42393.herokuapp.com/",
+            github: "https://github.com/wsbousman/Gripe-Support"
         },
         {
-            name: "Calorie",
-            description: "LET'S GET THIS BREAD BABY, TRACK THOSE CALORIES, TRACK THOSE HANDSSSSSS",
-            image: ""
+            name: "Gripe Support",
+            technology: "Sequelize, Handlebars, Express, Node",
+            description: "Gripe support was designed as a space where one could gripe or support other user's anonymously. Visitors can create an account that will allow them to make posts (categorized as a gripe or an encouragement). Additionally, user's can 'hug' and comment on posts.",
+            image: "gs",
+            website: "https://infinite-bayou-42393.herokuapp.com/",
+            github: "https://github.com/wsbousman/Gripe-Support"
         },
+        {
+            name: "Gripe Support",
+            technology: "Sequelize, Handlebars, Express, Node",
+            description: "Gripe support was designed as a space where one could gripe or support other user's anonymously. Visitors can create an account that will allow them to make posts (categorized as a gripe or an encouragement). Additionally, user's can 'hug' and comment on posts.",
+            image: "gs",
+            website: "https://infinite-bayou-42393.herokuapp.com/",
+            github: "https://github.com/wsbousman/Gripe-Support"
+        },
+        {
+            name: "Gripe Support",
+            technology: "Sequelize, Handlebars, Express, Node",
+            description: "Gripe support was designed as a space where one could gripe or support other user's anonymously. Visitors can create an account that will allow them to make posts (categorized as a gripe or an encouragement). Additionally, user's can 'hug' and comment on posts.",
+            image: "gs",
+            website: "https://infinite-bayou-42393.herokuapp.com/",
+            github: "https://github.com/wsbousman/Gripe-Support"
+        }
     ];
 
     const developing = [
