f5baafc7b6058e40cff97a9cff3b9dd1270ddaa5
AshisPatel <ashis.n.patel@gmail.com>
2021-10-29 11:41:01 -0500

    Add validateEmail and validateNumber helper functions for form input validation

--- src/Components/ContactModal.js
+++ src/Components/ContactModal.js
@@ -1,4 +1,7 @@
 import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import validateEmail from "../utils/validateEmail";
+import validateNumber from "../utils/validateNumber";
 
 function ContactModal(props) {
 
@@ -6,27 +9,50 @@ function ContactModal(props) {
         name: "",
         email: "",
         phone: "",
-        message: "Enter a message here...",
+        message: "Enter a message here... (compliments are always welcome (^^)b)",
         permission: "no",
         contactViaPhone: false,
-        contactViaEmail: false
+        contactViaEmail: false,
     });
 
-    const { name, email, phone, contactViaPhone, contactViaEmail, message, permission } = formState;
+    const [errMsg, setErrMsg] = useState('');
+
+    const { name, email, phone, contactViaPhone, contactViaEmail, message, permission} = formState;
 
     const handleChange = (e) => {
         const { name, value, type, checked } = e.target;
+
+        if(name==="email") {
+            !validateEmail(value) ? setErrMsg('The input email is invalid.') : null;
+            // return; 
+        }
+
+        if(name==="number") {
+            !validateNumber(value) && setErrMsg('The input phone number is invalid.')
+            return; 
+        }
+
+        if(value.length === 0) {
+            
+        }
+
         type === "checked" ?
             setFormState({ [name]: checked }) :
             setFormState({ [name]: value });
     }
 
+    const validateForm = (e) => {
+        e.preventDefault();
+        
+    }
+
     return (
         <div id="contact-form-wrapper"> 
-            <form id="contact-form" className="d-flex flex-column p-3 justify-content-around  w-50 mx-auto">
+            <form id="contact-form" className="d-flex flex-column p-3 justify-content-around  w-50 mx-auto" onSubmit={validateForm}>
                 <label for="name" > Name:</label>
                     <input
                     className=""
+                    autoComplete="off"
                     name="name"
                     type="text"
                     value={name}
@@ -36,6 +62,7 @@ function ContactModal(props) {
            
                 <label for="email"> Email: </label>
                     <input
+                        autoComplete="off"
                         name="email"
                         type="email"
                         value={email}
@@ -44,7 +71,8 @@ function ContactModal(props) {
                     />
                 <label for="phone-number"> Phone #:</label>
                     <input
-                        className="mb-2"
+                        className="mb-3"
+                        autoComplete="off"
                         name="phone-number"
                         type="tel"
                         value={phone}
@@ -54,7 +82,10 @@ function ContactModal(props) {
                     />
                 <textarea
                     name="message"
+                    autoComplete="off"
                     value={message}
+                    rows="5"
+                    className="mb-2"
                     onChange={handleChange}
                 />
                <label> Would you like me to contact you?
@@ -95,7 +126,14 @@ function ContactModal(props) {
                         </label>
                     </div>
                 }
-
+                {
+                    errMsg &&
+                    <p className="form-err-msg my-2">
+                        {errMsg}
+                    </p>
+                }
+                
+                <button className="button mt-2" type="submit"> Send Message <FontAwesomeIcon icon="paper-plane" /> </button>
             </form>
             <div id="contact-form-backdrop"></div>
         </div>
