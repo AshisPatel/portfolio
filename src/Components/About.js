8af995fc22bc74c5e02686e00f039006aa9ee6d8
AP <ashis.n.patel@gmail.com>
2021-10-31 16:47:34 -0500

    Add animation effects and loadin to the about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -13,17 +13,19 @@ function About(props) {
             <div className="row">
                 <img src={require("../assets/images/me.JPG").default} alt="Ashis striking a pose that embodies his grace and magnificence" className="col-12 col-md-3 ps-4 ps-md-0 pe-4 pe-md-2 h-md-100 mb-md-0 mb-4" id="about-img" />
                 <div className=" col-10 offset-1 offset-md-0 col-md-9 about-info-box d-flex flex-column justify-content-around">
-                    <h1>Hello friend! My name is Ashis Patel.</h1>
+                    <h1 id="welcome-statement">Hello friend! My name is Ashis Patel.</h1>
                     <p className="about-me-intro">
                         I am a Full Stack Developer that is passionate about creating web applications that improve the lives of users! I recieved my Bachelor's Degree in Chemical Engineering from Texas A&M University in 2018 and recently recieved a Full Stack Certificate from the University of Texas. I have a background in consulting and teaching, where I have solved hundreds of engineering problems, taught dozens of classes and even developed my own training course. I've made many happy clients in my career and I look forward to making many more in the field of web development!
 
                     </p>
 
-                    <p className="fs-5">
-                        Feel free to check out my projects, github, resume, or reach out to me using the buttons below!
-                    </p>
+
                     <div className="container">
-                        <div className="row mb-3">
+
+                        <div className="row mb-3 about-link-box">
+                            <p className="fs-5">
+                                Feel free to check out my projects, github, resume, or reach out to me using the buttons below!
+                            </p>
                             <Link to="/projects" className="col-12 col-md-6"><button className="button w-100 mb-2">Projects <FontAwesomeIcon icon="code-branch" /></button></Link>
 
                             <a href="https://github.com/AshisPatel" target="_blank" className="col-12 col-md-6" rel="noreferrer">
