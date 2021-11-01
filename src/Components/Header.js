2c1565d01ac88db78f368a67fbfe7ba69bb6d2b2
AshisPatel <ashis.n.patel@gmail.com>
2021-11-01 09:37:29 -0500

    Change styling on navbar links

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -17,11 +17,11 @@ function Header(props) {
             { width > 767 ?
                 <nav className="mt-2">
                     <ul className="d-flex px-auto justify-content-between">
-                        <Link to="/"><button className="button me-3"><FontAwesomeIcon icon="grin-beam" /> About Me</button></Link>
-                        <Link to="/projects"><button className="button me-3"><FontAwesomeIcon icon="code-branch" /> Projects</button></Link>
-                        <Link to="/contact"><button className="button me-3"><FontAwesomeIcon icon="address-card" /> Contact</button></Link>
-                        <a href={require("../assets/files/apr.pdf").default} target="_blank" rel="noreferrer">
-                                    <button className="button me-3"><FontAwesomeIcon icon="file" /> Resume </button>
+                        <Link to="/" className="nav-link"><FontAwesomeIcon icon="grin-beam" /> About Me</Link>
+                        <Link to="/projects" className="nav-link"><FontAwesomeIcon icon="code-branch" /> Projects</Link>
+                        <Link to="/contact" className="nav-link"><FontAwesomeIcon icon="address-card" /> Contact</Link>
+                        <a href={require("../assets/files/apr.pdf").default} target="_blank" rel="noreferrer" className="nav-link me-2">
+                                    <FontAwesomeIcon icon="file" /> Resume
                         </a>
                     </ul>
                 </nav> 
