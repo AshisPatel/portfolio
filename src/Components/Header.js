7b8f79696166342735f5df2dea282d3ba4287520
AP <ashis.n.patel@gmail.com>
2021-10-31 21:55:54 -0500

    Add hook for getting window dimensions

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import useWindowDimensions from "../hooks/useWindowDimensions";
 
 function Header(props) {
+
+    const { height, width } = useWindowDimensions();
+
     return (
         <header className="d-md-flex justify-content-between pt-2">
             
             <h2 id="home" className="ps-md-2 text-center text-md-left">
                 Ashis Patel 
             </h2>
-
+            <p>Height: {height} and Width: {width}</p>
             <nav className="mt-2">
                 <ul className="d-flex px-auto justify-content-between">
                     <Link to="/"><button className="button me-3"><FontAwesomeIcon icon="grin-beam" /> About Me</button></Link>
