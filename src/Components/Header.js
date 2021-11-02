685497044895ddd4371c0671bc5246937851a46a
AP <ashis.n.patel@gmail.com>
2021-11-01 21:07:42 -0500

    Input in animated logo

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,43 +6,56 @@ import useWindowDimensions from "../hooks/useWindowDimensions";
 function Header(props) {
 
     const { height, width } = useWindowDimensions();
-
-    // const [location, setLocation] = useState(window.location.pathname);
     const {location, setLocation} = props; 
     const [display, setDisplay] = useState(true);
+    const [isHovered, setIsHovered] = useState(false); 
+
+    const transitionWidth = 768; 
 
     const handleClick = (e) => {
-        width <= 767 && setDisplay(false);
+        width <= transitionWidth && setDisplay(false);
         const pathName = e.target.getAttribute('href');
         setLocation(pathName);
     }
 
     useEffect(() => {
-        width > 767 ? setDisplay(true) : setDisplay(false);
+        width > transitionWidth ? setDisplay(true) : setDisplay(false);
     }, [width])
 
     const revealDropdown = () => {
         setDisplay(prevDisplay => !prevDisplay);
     }
 
-    const ulClass = width > 767 ? "d-flex px-auto justify-content-between" : "d-flex flex-column ps-0 mb-0";
-    const navLinkPadding = width > 767 ? "mx-1" : "mx-1 my-1"
+    const ulClass = width > transitionWidth ? "d-flex px-auto justify-content-between" : "d-flex flex-column ps-0 mb-0";
+    const navLinkPadding = width > transitionWidth ? "mx-1" : "mx-1 my-1"
+
+    const firstLetter = "<A";
+    const firstName = "shis";
+    const lastLetter = "P"
+    const lastName = "atel"
+    const endBracket = ">";
 
     return (
         <header className="d-flex justify-content-between pt-2 px-4">
 
             <h2 id="home" className="ps-md-2 text-center text-md-left">
-                <div class="d-flex flex-column" id="logo-wrapper">
-                    <span id="first-name">Ashis</span>
-                    <span id="last-name">Patel</span>
+                <div className="d-flex" id="logo-wrapper" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
+                    <span id="first-initial" className={isHovered ? "move-left" : undefined }>{firstLetter}
+                        {isHovered && <span id="first-name">{firstName}</span>}
+                    </span>
+                    <span id="slash">/</span>
+                    <span id="last-initial">{lastLetter}
+                        {isHovered && <span id="last-name">{lastName}</span>}
+                    </span>
+                    <span id="end-bracket" className={isHovered ? "move-right" : undefined }>{endBracket}</span>
                 </div>
             </h2>
             <div className="dropdown-container">
-                {width <= 767 &&
+                {width <= transitionWidth &&
                     <button className="dropdown-button" onClick={() => revealDropdown()}><FontAwesomeIcon icon={display ? "times" : "bars"} /></button>
                 }
                 {display &&
-                    <nav className={`mt-2 ${width <= 767 && "dropdown p-2"}`}>
+                    <nav className={`mt-3 ${width <= transitionWidth && "dropdown p-2"}`}>
 
 
                         <ul className={ulClass}>
