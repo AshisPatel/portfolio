c375cbf79a3255700e478cb7a043c06fbda8fc17
AP <ashis.n.patel@gmail.com>
2021-11-02 18:44:10 -0500

    change text alignment to be responsive on about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -22,16 +22,16 @@ function About(props) {
                 <div className=" col-10 offset-1 offset-md-0 col-md-9 about-info-box d-flex flex-column justify-content-around pt-2">
                     <div className="container">
                         <div className="row">
-                            <h2 id="welcome-statement">Hello friend, I'm Ashis Patel!</h2>
+                            <h2 id="welcome-statement" className="text-center text-md-start" >Hello friend, I'm Ashis Patel!</h2>
                         </div>
 
                         <div className="row">
-                            <p className="about-me-intro">
+                            <p className="about-me-intro fs-5 text-center text-md-start">
                                 A Full Stack Developer who is passionate about creating web applications that improve the lives of users. 
                                 I recieved my Bachelor's Degree in Chemical Engineering from Texas A&M University in 2018 and recently recieved a Full Stack Certificate from the University of Texas. 
                             </p>
                             <hr/>
-                            <p className="more-intro">
+                            <p className="more-intro fs-5 text-center text-md-start">
                                 Through my years as a consultant, I have solved hundreds of engineering problems, taught dozens of classes and even developed my own training course. 
                                 I've made many happy clients in my career and I look forward to making many more in the field of web development!  
                             </p>
@@ -42,7 +42,7 @@ function About(props) {
                     <div className="container">
 
                         <div className="row mb-3 about-link-box">
-                            <p className="fs-5">
+                            <p className="fs-5 text-center text-md-start">
                                 Feel free to check out my projects, github, resume, or reach out to me using the buttons below!
                             </p>
                             <Link to="/projects" className="col-12 col-md-6" onClick={handleClick}><button className="button w-100 mb-2" data-link="/projects"><FontAwesomeIcon icon="code-branch" /> Projects</button></Link>
@@ -64,7 +64,7 @@ function About(props) {
             <div className="row mt-2">
 
                 <div className="about-extra-info mb-2 col-10 offset-1 col-md-12 offset-md-0" onMouseEnter={() => setIsHovered(true)} onMouseLeave={() => setIsHovered(false)}>
-                    <h3 className="text-center">Fun Facts On Hover!</h3>
+                    <h3 className="text-center">Fun Facts About Me On Hover!</h3>
 
                     {
                         isHovered &&
