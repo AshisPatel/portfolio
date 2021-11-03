f1bb18c29158d5cc11d523fc0ba1fa586ead522c
AP <ashis.n.patel@gmail.com>
2021-11-02 22:16:56 -0500

    Change BrowserRouter to HashRouter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter as Router} from "react-router-dom";
+import {HashRouter as Router} from "react-router-dom";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import "../src/style.css"
 import { library } from '@fortawesome/fontawesome-svg-core';
