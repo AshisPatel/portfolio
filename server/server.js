df205957158a2d513f25bea9c0ea85585c406ead
AP <ashis.n.patel@gmail.com>
2021-11-15 23:16:25 -0600

    Remove homepage from package.json

--- server/server.js
+++ server/server.js
@@ -7,11 +7,13 @@ const app = express();
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
-app.use(routes);
 
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.use(routes);
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}! (^^)b`)
